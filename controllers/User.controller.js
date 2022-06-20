3984a1a5ba8c7feb9509cf39773ce3d6b451eb96
Erhan Yaylalı <32177766+erhanyaylali1@users.noreply.github.com>
2022-06-20 19:43:23 +0300

    new reset link is configured

--- controllers/User.controller.js
+++ controllers/User.controller.js
@@ -232,7 +232,7 @@ class AuthController {
       if (!user) return res.status(404).json({ error: 'User not found' });
       else {
         const token = nanoid();
-        const link = `${process.env.CLIENT_URL}/reset/${token}`;
+        const link = `${process.env.CLIENT_URL}/#reset/${token}`;
         const html = `<h1>Password Reset</h1>
             <p>You are receiving this email because you (or someone else) have requested the reset of the password for your account.
             Please click on the following link, or paste this into your browser to complete the process:</p>
